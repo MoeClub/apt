e24c7ff440b4495029898e3276afe758d1d6970a
MoeClub <32381827+MoeClub@users.noreply.github.com>
2021-08-19 17:03:01 +0800

    Update Net.sh

--- Net.sh
+++ Net.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
+
+function dependence(){
+  Full='0';
+  for BIN_DEP in `echo "$1" |sed 's/,/\n/g'`
+    do
+      if [[ -n "$BIN_DEP" ]]; then
+        Found='0';
+        for BIN_PATH in `echo "$PATH" |sed 's/:/\n/g'`
+          do
+            ls $BIN_PATH/$BIN_DEP >/dev/null 2>&1;
+            if [ $? == '0' ]; then
+              Found='1';
+              break;
+            fi
+          done
+        if [ "$Found" == '1' ]; then
+          echo -en "[\033[32mok\033[0m]\t";
+        else
+          Full='1';
+          echo -en "[\033[31mNot Install\033[0m]";
+        fi
+        echo -en "\t$BIN_DEP\n";
+      fi
+    done
+  if [ "$Full" == '1' ]; then
+    echo -ne "\n\033[31mError! \033[0mPlease use '\033[33mapt-get\033[0m' or '\033[33myum\033[0m' install it.\n\n\n"
+    exit 1;
+  fi
+}
+
 function netmask() {
   n="${1:-32}"
   b=""
