59e26e7ca494ad39e68cc94819a4a48490ded023
MoeClub <32381827+MoeClub@users.noreply.github.com>
2021-08-19 17:22:27 +0800

    Update Net.sh

--- Net.sh
+++ Net.sh
@@ -1,5 +1,143 @@
 #!/bin/bash
 
+## License: GPL
+## It can reinstall Debian, Ubuntu, CentOS system with network.
+## Default root password: MoeClub.org
+## Blog: https://moeclub.org
+## Written By MoeClub.org
+
+
+export tmpVER=''
+export tmpDIST=''
+export tmpURL=''
+export tmpWORD=''
+export tmpMirror=''
+export tmpSSL=''
+export ipAddr=''
+export ipMask=''
+export ipGate=''
+export Relese=''
+export ddMode='0'
+export setNet='0'
+export setRDP='0'
+export setIPv6='0'
+export isMirror='0'
+export FindDists='0'
+export loaderMode='0'
+export IncFirmware='0'
+export SpikCheckDIST='0'
+export setInterfaceName='0'
+export UNKNOWHW='0'
+export UNVER='6.4'
+
+while [[ $# -ge 1 ]]; do
+  case $1 in
+    -a|--auto)
+      shift
+      ;;
+    -m|--manual)
+      shift
+      ;;
+    -v|--ver)
+      shift
+      tmpVER="$1"
+      shift
+      ;;
+    -d|--debian)
+      shift
+      Relese='Debian'
+      tmpDIST="$1"
+      shift
+      ;;
+    -u|--ubuntu)
+      shift
+      Relese='Ubuntu'
+      tmpDIST="$1"
+      shift
+      ;;
+    -c|--centos)
+      shift
+      Relese='CentOS'
+      tmpDIST="$1"
+      shift
+      ;;
+    -dd|--image)
+      shift
+      ddMode='1'
+      tmpURL="$1"
+      shift
+      ;;
+    -p|--password)
+      shift
+      tmpWORD="$1"
+      shift
+      ;;
+    -i|--interface)
+      shift
+      interface="$1"
+      shift
+      ;;
+    --ip-addr)
+      shift
+      ipAddr="$1"
+      shift
+      ;;
+    --ip-mask)
+      shift
+      ipMask="$1"
+      shift
+      ;;
+    --ip-gate)
+      shift
+      ipGate="$1"
+      shift
+      ;;
+    --dev-net)
+      shift
+      setInterfaceName='1'
+      ;;
+    --loader)
+      shift
+      loaderMode='1'
+      ;;
+    -apt|-yum|--mirror)
+      shift
+      isMirror='1'
+      tmpMirror="$1"
+      shift
+      ;;
+    -rdp)
+      shift
+      setRDP='1'
+      WinRemote="$1"
+      shift
+      ;;
+    -ssl)
+      shift
+      tmpSSL="$1"
+      shift
+      ;;
+    -firmware)
+      shift
+      IncFirmware="1"
+      ;;
+    -port)
+      shift
+      sshPORT="$1"
+      shift
+      ;;
+    --ipv6)
+      shift
+      setIPv6='1'
+      ;;
+    *)
+      if [[ "$1" != 'error' ]]; then echo -ne "\nInvaild option: '$1'\n\n"; fi
+      echo -ne " Usage:\n\tbash $(basename $0)\t-d/--debian [\033[33m\033[04mdists-name\033[0m]\n\t\t\t\t-u/--ubuntu [\033[04mdists-name\033[0m]\n\t\t\t\t-c/--centos [\033[33m\033[04mdists-verison\033[0m]\n\t\t\t\t-v/--ver [32/\033[33m\033[04mi386\033[0m|64/amd64]\n\t\t\t\t--ip-addr/--ip-gate/--ip-mask\n\t\t\t\t-apt/-yum/--mirror\n\t\t\t\t-dd/--image\n\t\t\t\t-a/--auto\n\t\t\t\t-m/--manual\n"
+      exit 1;
+      ;;
+    esac
+  done
+
 [[ "$EUID" -ne '0' ]] && echo "Error:This script must be run as root!" && exit 1;
 
 function dependence(){
