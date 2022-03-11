57a54b17ca9a154727ebb5abd048db3c42707e51
MoeClub <32381827+MoeClub@users.noreply.github.com>
2022-03-11 14:56:28 +0800

    Update bbr.sh

--- bbr/bbr.sh
+++ bbr/bbr.sh
@@ -17,34 +17,35 @@ cat >/etc/sysctl.conf<<EOF
 fs.file-max = 104857600
 fs.nr_open = 1048576
 vm.overcommit_memory = 1
-net.ipv4.ip_forward = 1
-net.core.somaxconn = 4096
+net.core.somaxconn = 65535
 net.core.optmem_max = 262144
 net.core.rmem_max = 8388608
 net.core.wmem_max = 8388608
 net.core.rmem_default = 262144
 net.core.wmem_default = 262144
 net.core.netdev_max_backlog = 65536
-net.ipv4.tcp_mem = 262144 6291456 8388608
-net.ipv4.tcp_rmem = 16384 262144 8388608
-net.ipv4.tcp_wmem = 8192 262144 8388608
+net.ipv4.tcp_mem = 4096 262144 8388608
+net.ipv4.tcp_rmem = 4096 262144 8388608
+net.ipv4.tcp_wmem = 4096 262144 8388608
 net.ipv4.tcp_syncookies = 1
-net.ipv4.tcp_syn_retries = 4
-net.ipv4.tcp_synack_retries = 3
-net.ipv4.tcp_max_syn_backlog = 4096
-net.ipv4.tcp_fin_timeout = 24
+net.ipv4.tcp_syn_retries = 3
+net.ipv4.tcp_synack_retries = 2
+net.ipv4.tcp_max_syn_backlog = 65535
+net.ipv4.tcp_fin_timeout = 16
 net.ipv4.tcp_keepalive_intvl = 32
 net.ipv4.tcp_keepalive_probes = 3
 net.ipv4.tcp_keepalive_time = 900
 net.ipv4.tcp_retries1 = 3
 net.ipv4.tcp_retries2 = 8
 net.ipv4.icmp_echo_ignore_all = 1
+net.ipv4.tcp_no_metrics_save = 1
 net.ipv4.tcp_timestamps = 1
 net.ipv4.tcp_slow_start_after_idle = 0
 # net.ipv4.tcp_fastopen = 3
 net.core.default_qdisc = fq
 net.ipv4.tcp_congestion_control = bbr
 net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv4.ip_forward = 1
 
 EOF
 
