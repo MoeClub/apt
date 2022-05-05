1ac112755d3cae48092e31f4fa03ee2247ac710a
MoeClub <32381827+MoeClub@users.noreply.github.com>
2022-05-05 11:10:36 +0800

    Update bbr.sh

--- bbr/bbr.sh
+++ bbr/bbr.sh
@@ -38,17 +38,26 @@ net.ipv4.tcp_keepalive_probes = 3
 net.ipv4.tcp_keepalive_time = 900
 net.ipv4.tcp_retries1 = 3
 net.ipv4.tcp_retries2 = 8
-net.ipv4.icmp_echo_ignore_all = 1
 net.ipv4.tcp_no_metrics_save = 1
 net.ipv4.tcp_timestamps = 1
 net.ipv4.tcp_slow_start_after_idle = 0
-# net.ipv4.tcp_fastopen = 3
-net.core.default_qdisc = fq
-net.ipv4.tcp_congestion_control = bbr
-net.ipv6.conf.all.disable_ipv6 = 1
 net.ipv4.ip_forward = 1
 
+net.ipv4.icmp_echo_ignore_all = 1
+net.ipv6.conf.all.disable_ipv6 = 1
+
+net.ipv4.tcp_fastopen = 0
+net.ipv4.tcp_fack = 1
+net.ipv4.tcp_sack = 1
+net.ipv4.tcp_dsack = 1
+net.ipv4.tcp_ecn = 0
+net.ipv4.tcp_ecn_fallback = 1
+
+net.core.default_qdisc = fq
+net.ipv4.tcp_congestion_control = bbr
+
 EOF
+
 
 [ "$REBOOT" -eq "1" ] && reboot
 
