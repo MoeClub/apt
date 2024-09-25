08b09f842c1c5c858873a5b74d703a4c4af09a2b
MoeClub <32381827+MoeClub@users.noreply.github.com>
2024-09-25 10:57:29 +0800

    Update offline.sh

--- docker/offline.sh
+++ docker/offline.sh
@@ -5,14 +5,17 @@ image="${1:-}"
 name="${2:-}"
 [ -n "$image" ] || exit 1
 work="$(mktemp -d)"
+trap "rm -rf ${work}" EXIT
 
 docker pull "$image" >/dev/null 2>&1
 [ $? == 0 ] || exit 1
 
-cd "${work}"
 docker save -o "${work}/_" "$image" >/dev/null 2>&1
+[ $? == 0 ] || exit 1
 # docker images -a -q |xargs docker rmi -f >/dev/null 2>&1
 
 split -b "$size" -d "${work}/_" "$name"
-[ $? == 0 ] && echo "${work}" && rm -rf "${work}/_"  && exit 0 || rm -rf "${work}"
-exit 1
+[ $? == 0 ] || exit 1
+trap "rm -rf ${work}/_" EXIT
+echo "${work}"
+
