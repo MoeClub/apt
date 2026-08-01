f0ed32abf78b7eb59fa33e2aa4b77a1cb2a7bc7d
MoeClub <32381827+MoeClub@users.noreply.github.com>
2026-08-01 19:20:00 +0800

    Update nginx_buildx.sh

--- nginx/nginx_buildx.sh
+++ nginx/nginx_buildx.sh
@@ -5,7 +5,6 @@ dockerProject="nginx"
 dockerBase="alpine:3.20"
 dockerName="${dockerProject}_buildx"
 
-manifest=""
 for arch in "amd64" "arm64"; do
   docker run --rm --platform "linux/${arch}" "${dockerBase}" apk --print-arch >/dev/null 2>&1 || docker run --privileged --rm tonistiigi/binfmt --install "${arch}"
   docker rm -f "${dockerName}" >/dev/null 2>&1 || true
