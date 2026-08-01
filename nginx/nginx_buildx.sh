8e510d7d87d218ab143ad9928c006bf87c57fdbc
MoeClub <32381827+MoeClub@users.noreply.github.com>
2026-08-01 19:15:43 +0800

    Create nginx_buildx.sh

---
+++ nginx/nginx_buildx.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+dockerBase="alpine:3.20"
+dockerName="${dockerProject}_buildx"
+
+manifest=""
+for arch in "amd64" "arm64"; do
+  docker run --rm --platform "linux/${arch}" "${dockerBase}" apk --print-arch >/dev/null 2>&1 || docker run --privileged --rm tonistiigi/binfmt --install "${arch}"
+  docker rm -f "${dockerName}" >/dev/null 2>&1 || true
+  docker run --rm --platform "linux/${arch}" --name "${dockerName}" -it -v /mnt:/mnt "${dockerBase}" /bin/sh /mnt/nginx_musl.sh
+  docker run --rm --platform "linux/${arch}" --name "${dockerName}" -it -v /mnt:/mnt "${dockerBase}" /bin/sh /mnt/nginx_musl.sh 1
+done
