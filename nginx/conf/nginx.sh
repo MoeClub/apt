360b820d4599f3209662a27adb75e3a6bc94aec4
MoeClub <32381827+MoeClub@users.noreply.github.com>
2026-08-02 17:18:03 +0800

    Update nginx.sh

--- nginx/conf/nginx.sh
+++ nginx/conf/nginx.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 [ -d /etc/nginx ] || exit 1
-nginxVersion=1.22.1
+nginxVersion="1.30.4_ja3"
+
+src="https://github.com/MoeClub/apt/raw/refs/heads/master/nginx"
 
 # config
 find /etc/nginx -type f,l |xargs rm -rf
 find /etc/nginx -maxdepth 1 -type d -empty |xargs rm -rf
 mkdir -p /etc/nginx/conf.d /etc/nginx/sites-available
-wget -qO /etc/nginx/nginx.conf https://github.com/MoeClub/apt/raw/master/nginx/conf/nginx.conf
-wget -qO /etc/nginx/sites-available/default https://github.com/MoeClub/apt/raw/master/nginx/conf/default
+wget -qO /etc/nginx/nginx.conf "${src}/conf/nginx.conf"
+wget -qO /etc/nginx/sites-available/default "${src}/conf/default"
 
 # nginx
 case `uname -m` in aarch64|arm64) VER="arm64";; x86_64|amd64) VER="amd64";; *) VER="";; esac
 [ -n "$VER" ] || exit 1;
 nginxBin=`which nginx`
 mv "$nginxBin" "${nginxBin}.bak"
-wget -qO "$nginxBin" "https://raw.githubusercontent.com/MoeClub/apt/master/nginx/nginx_${VER}_v${nginxVersion}"
+wget -qO "$nginxBin" "${src}/nginx_${VER}_v${nginxVersion}"
 chmod 755 "$nginxBin"
 
 # restart nginx
