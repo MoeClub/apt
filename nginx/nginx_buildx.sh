8a2c08ac6c952e1b094a97732a0e023a173fb285
MoeClub <32381827+MoeClub@users.noreply.github.com>
2026-08-03 01:26:55 +0800

    Update nginx_buildx.sh

--- nginx/nginx_buildx.sh
+++ nginx/nginx_buildx.sh
@@ -8,6 +8,6 @@ dockerName="${dockerProject}_buildx"
 for arch in "amd64" "arm64"; do
   docker run --rm --platform "linux/${arch}" "${dockerBase}" apk --print-arch >/dev/null 2>&1 || docker run --privileged --rm tonistiigi/binfmt --install "${arch}"
   docker rm -f "${dockerName}" >/dev/null 2>&1 || true
-  docker run --rm --platform "linux/${arch}" --name "${dockerName}" -it -v /mnt:/mnt "${dockerBase}" /bin/sh /mnt/nginx_musl.sh
+  docker run --rm --platform "linux/${arch}" --name "${dockerName}" -it -v /mnt:/mnt "${dockerBase}" /bin/sh /mnt/nginx_musl.sh 0
   docker run --rm --platform "linux/${arch}" --name "${dockerName}" -it -v /mnt:/mnt "${dockerBase}" /bin/sh /mnt/nginx_musl.sh 1
 done
